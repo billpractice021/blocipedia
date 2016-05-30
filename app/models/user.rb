312b95cf83b990731f02a63ed1a53d1dbb9a5059
Charlie Gaines <beaugaines@gmail.com>
2016-05-30 14:13:20 -0400

    basic preview working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+        
   has_many :wikis, dependent: :destroy
 
   has_many :collaborations
@@ -11,5 +12,5 @@ class User < ActiveRecord::Base
   
   has_many :charges
 
-  enum role: {standard: 0, premium: 1, admin: 2}
+  enum role: { standard: 0, premium: 1, admin: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160522233513) do
+ActiveRecord::Schema.define(version: 20160530174702) do
 
   create_table "charges", force: :cascade do |t|
     t.integer  "user_id"
@@ -50,10 +50,14 @@ ActiveRecord::Schema.define(version: 20160522233513) do
     t.datetime "updated_at",                          null: false
     t.string   "stripe_id"
     t.integer  "role",                   default: 0
+    t.string   "slug"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
